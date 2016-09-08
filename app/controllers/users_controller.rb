656622fae9de73e458d28a5914bef02bd32bbfe6
tylertaylor <vikingtyty@gmail.com>
2016-09-08 15:57:13 -0500

    shelters index links to individual show pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,7 +27,6 @@ class UsersController < ApplicationController
   end
 
   post '/login' do
-    binding.pry
     user = User.find_by(name: params[:name])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
