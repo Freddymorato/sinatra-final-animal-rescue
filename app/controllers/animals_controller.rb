72ef20212e3a32885c67fe9413e2e6aec42dcbda
tylertaylor <vikingtyty@gmail.com>
2016-09-10 10:05:50 -0500

    started animal create actions, added link for new animal on animals index

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,7 +1,19 @@
 class AnimalsController < ApplicationController
 
+  ### ANIMALS INDEX ACTION ###
+
   get '/animals' do
     erb :'animals/index'
+  end
+
+  ### CREATE ACTIONS ###
+
+  get '/animals/new' do
+    "new animals"
+  end
+
+  post '/animals' do
+
   end
 
 end
